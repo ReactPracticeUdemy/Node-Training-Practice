3596e3b486e7b614a5419050e41d6e1f83944f27
Rajshrishrimalshah <raj.shrishrimalshah@successive.tech>
2019-06-21 07:23:03 +0530

    feat: extra code removed from src/trainee/controller.ts

--- src/controllers/trainee/controller.ts
+++ src/controllers/trainee/controller.ts
@@ -1,20 +1,13 @@
-import * as express from 'express';
 import { Request, Response } from 'express';
 class TraineeController {
   public get(req: Request, res: Response) {
     res.send('get method called');
   }
 
-  public post(req: express.Request, res: express.Response) {
-    //const { id, name, email } = req.body ;
-    let id = req.body.id;
-    // let name = req.body.name;
-    // let email = req.body.email;
-    console.log('Function called');
-
-    // console.log(`ID: ${id}, Name: ${name}, Email: ${email}`);
-
-    res.send("post method called", id);
+  public post(req: Request, res: Response) {
+    const { id, name, email } = req.body ;
+    console.log(`ID: ${id}, Name: ${name}, Email: ${email}`);
+    res.send("post method called");
   }
 
   public put(req: Request, res: Response) {
